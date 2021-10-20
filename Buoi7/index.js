86748ae92a8f6a631b35793ea9c8380b67a957d2
huongcucnguyen <huongcucnguyen0889@gmail.com>
2021-10-20 19:19:05 +0700

    Add Buoi7

---
+++ Buoi7/index.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("Chao ca lop");
+console.log("Da updated");
+//alert là chức năng của chrome hỗ trợ nên node dưới local không hoạt động
+alert("Thong bao cho nguoi dung");
